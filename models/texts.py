a8647fb845f800649022f685e4a9e97a6c1d0bd0
DESKTOP-C3TQE37\uws06 <negioisy@gmail.com>
2025-04-07 21:01:25 +0900

    test

--- models/texts.py
+++ models/texts.py
@@ -1,13 +1,16 @@
-from app_instance import db
+from app_instance import db, app
 from sqlalchemy.dialects.mysql import VARCHAR, INTEGER
 
 class Text(db.Model):
     __tablename__ = 'texts'
     id = db.Column("id", INTEGER, primary_key=True)
-    text = db.Column("text", VARCHAR, nullable=False)
+    text = db.Column("text", VARCHAR(255), nullable=False)
     
     def __init__(self, text=None):
         self.text = text
 
     def __repr__(self):
         return f'<Text {self.text}>'
+
+with app.app_context():
+    db.create_all()
